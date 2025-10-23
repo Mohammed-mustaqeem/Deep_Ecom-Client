eb7620b52956bb52f0dbef2568838502154024d9
Mohammed Mustaqeem <mustaqeemcode@gmail.com>
2025-10-24 01:02:28 +0530

    popular-done

--- src/components/PopularProduct.jsx
+++ src/components/PopularProduct.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import Title from "./Title";
+import Item from "./Item";
+import { useAppContext } from "../context/AppContext";
 const PopularProduct = () => {
-  return (
-    <div>
-      Popular Products
-    </div>
-  )
-}
 
-export default PopularProduct
+  const [popularProducts, setPopularProducts] = useState([]);
+  const { product } = useAppContext();
+
+  useEffect(() => {
+    const data = product.filter((item) => item.popular && item.inStock);
+    setPopularProducts(data.slice(0, 4));
+  }, [product]);
+
+
+  return (
+    <section className="max-padd-container mt-28">
+      <Title title1={"Popular"} title2={"Products"} titleStyles={"pb-10"} />
+      {/* Container */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        {popularProducts.map((product) => (
+          <div key={product._id}>
+            <Item product={product} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PopularProduct;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Features from '../context/Features'
-import NewArrivals from '../components/NewArrivals'
-import PopularProduct from '../components/PopularProduct'
+import React from "react";
+import Hero from "../components/Hero";
+import Features from "../context/Features";
+import NewArrivals from "../components/NewArrivals";
+import PopularProduct from "../components/PopularProduct";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <Features/>
-      <NewArrivals/>
-      {/* <PopularProduct/> */}
-
+      <Hero />
+      <Features />
+      <NewArrivals />
+      <PopularProduct />
+      <div className="hidden sm:block  max-padd-container mt-28 bg-[url('/src/assets/banner.jpg')] bg-cover bg-center bg-no-repeat h-[300px]" />
+      <Testimonial />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
